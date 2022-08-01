4d1df93f18a204b141d7ce04900dc60c0299eaa8
Deborah Wade <sopranosamurai@yahoo.com>
2022-08-01 15:08:37 -0700

    added initial JS files

---
+++ js/questions.js
@@ -0,0 +1,46 @@
+/*all but 2 questions are modified from w3schools JavaScript quiz*/
+const quizQuestions = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answerOptions: {
+            a: "<js>",
+            b: "<javascript>",
+            c: "<script>",
+            d: "<scripting>"
+        },
+        answer: "c"
+    },
+    {
+        question: "Where is the correct place to insert a JavaScript?",
+        answerOptions: {
+            a: "Both <head> section and the <body> section are correct",
+            b: "The <body> section",
+            c: "The <head> section",
+            d: "The <footer> section"
+        },
+        answer: "a"
+    },
+    {
+        question: "What is the correct syntax for referring to an external script called \"xxx.js\"?",
+        answerOptions: {
+            a: "<script name =\"xxx.js\">",
+            b: "<script href = \"xxx.js\">",
+            c: "<script src=\"xxx.js\">",
+            d: "<script = \"xxx.js\">"
+        },
+        answer: "c"
+    },  
+
+    /* questions object literal template:
+    {
+        question: "",
+        answerOptions: {
+            a: "",
+            b: "",
+            c: "",
+            d: ""
+        },
+        answer: ""
+    },  
+    */
+];
