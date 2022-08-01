3a1cbc9210b1984fca6ef0c6729417fe2f90aa66
Deborah Wade <sopranosamurai@yahoo.com>
2022-08-01 16:59:30 -0700

    added -css files & normalize - frame for JS

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+//DOM Elements
+const startScreenEl = document.querySelector("#start-screen");
+const timeEl = document.querySelector("#time");
+const startButtonEl = 
+
+
+//when start button pressed: start game
+function startGame() {
+    //hide start screen
+    startScreenEl.classList.add("hide");
+    //Start the timer
+    startTimer();
+    //display each question and it's answer buttons
+    //evaluate answer
+    //If correct - display "correct" message
+    //If incorret:
+        //display "wrong" message 
+        //subtract 5 seconds from timer
+}
+//When timer is 0 or all questions asked:end game
+    //hide questions
+    //display score
+        //all correct answers + time left
+    //present form and sumbit button for initials
+    //display start over button
+
+function startTimer() {
+    
+}
