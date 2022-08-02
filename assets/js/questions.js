be2aded606ea1050f7b66f898fa500f12c62cabe
Deborah Wade <sopranosamurai@yahoo.com>
2022-08-01 22:29:56 -0700

    fixed the question display problems

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,35 +1,35 @@
-/*all but 2 questions are modified from w3schools JavaScript quiz*/
+/*all but 2 questions are modified from a w3schools JavaScript quiz*/
 const quizQuestions = [
     {
         question: "Inside which HTML element do we put the JavaScript?",
         answerOptions: {
-            a: "<js>",
-            b: "<javascript>",
-            c: "<script>",
-            d: "<scripting>"
+            a: "&lt;js&gt;",
+            b: "&lt;javascript&gt;",
+            c: "&lt;script&gt;",
+            d: "&lt;scripting&gt;"
         },
         answer: "c"
     },
     {
         question: "Where is the correct place to insert a JavaScript?",
         answerOptions: {
-            a: "Both <head> section and the <body> section are correct",
-            b: "The <body> section",
-            c: "The <head> section",
-            d: "The <footer> section"
+            a: "Both head section and the <body> section are correct",
+            b: "The body section",
+            c: "The head section",
+            d: "The footer section"
         },
         answer: "a"
     },
     {
         question: "What is the correct syntax for referring to an external script called \"xxx.js\"?",
         answerOptions: {
-            a: "<script name =\"xxx.js\">",
-            b: "<script href = \"xxx.js\">",
-            c: "<script src=\"xxx.js\">",
-            d: "<script = \"xxx.js\">"
+            a: "script name =\"xxx.js\"",
+            b: "script href = \"xxx.js\"",
+            c: "script src=\"xxx.js\"",
+            d: "script = \"xxx.js\""
         },
         answer: "c"
-    },  
+    } 
 
     /* questions object literal template:
     {
